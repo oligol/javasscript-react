5459ada8d705301bc8498b8e0a0d43285d0dfa22
oligol <oligol83@gmail.com>
2022-04-12 19:15:42 +0200

    010

---
+++ 10/js.js
@@ -0,0 +1,26 @@
+
+function calc(a,b) {
+    return (a + b);
+}
+console.log (calc(5,4));
+
+console.log (calc(8,4));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLocaleLowerCase());
+
+let twelv = "12.2px";
+
+// console.log(Math.round(twelv));
+console.log(parseInt(twelv));
+console.log(parseFloat(twelv));
