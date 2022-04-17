fc2df3a222bcca3f7507494e8ccf875d23ce6e01
oligol <oligol83@gmail.com>
2022-04-17 10:56:42 +0200

    013
    Масив і псевдомасив

--- 012/js.js
+++ 012/js.js
@@ -1,23 +1,86 @@
-let options = {
-    width: 1024,
-    height: 1024,
-    name: "test"
-};
+// let options = {
+//     width: 1024,
+//     height: 1024,
+//     name: "test"
+// };
 
-console.log(options.name);
+// console.log(options.name);
 
-options.bul = false;
-options.colors = {
-    border: "black",
-    bg: "red"
-};
+// options.bul = false;
+// options.colors = {
+//     border: "black",
+//     bg: "red"
+// };
 
-delete options.bul;
+// delete options.bul;
 
-console.log(options);
+// console.log(options);
 
-for (let key in options) {
-    console.log("Властивість " + key + " має значення " + options[key]);
-};
+// for (let key in options) {
+//     console.log("Властивість " + key + " має значення " + options[key]);
+// };
 
-console.log(Object.keys(options).length);
+// console.log(Object.keys(options).length);
+
+// let arr = ["first", 2, 3, "four", 5,];
+// // arr[99] = 99;
+
+// // arr.pop();
+// // arr.push("5");
+// // arr.shift();
+// // arr.unshift("1");
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr[i]);
+// // }
+
+// // console.log(arr.length);
+
+
+// arr.forEach(function(item, i, mass) {
+//     console.log(i + ': ' + item + "(масив: " + mass + ')');
+// });
+
+// console.log(arr);
+
+// let mass = [1,3,4,6,7];
+
+// for (let key in mass) { //ключ
+//     console.log(key);
+// }
+
+// for (let key of mass) { //значення
+//     console.log(key);
+// }
+
+// отримати данні від користувача
+// let ans = prompt("", "");
+//     arr = [];
+// arr = ans.split(',');
+// console.log(arr);
+
+// відправити на сервер
+
+let arr = ["aawe", "zas", "poe"],
+    i =arr.join(", ");
+    
+    console.log(arr);
+    console.log(i);
+
+
+// сортування стрічок
+
+let arr = ["aawe", "zas", "poe"],
+    i =arr.sort();
+    
+    console.log(arr);
+
+// сортування цифр
+
+
+let arr = [1, 15, 4],
+    i =arr.sort();
+function compareNum(a,b) {
+    return a-b;
+}
+    console.log(arr);
