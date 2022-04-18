1f3810dc6554ad0fcb6a10471ac6849b8dd164f4
oligol <oligol83@gmail.com>
2022-04-18 16:09:32 +0200

    011
    callback

--- 011/js.js
+++ 011/js.js
@@ -22,7 +22,7 @@ learnJS("JavaScript", function(){
 });
 
 
-
+/////////////////////////////
 function learnJS(lang, callback){
     console.log("Я учу " + lang);
     callback();
@@ -33,3 +33,14 @@ function done() {
 }
 
 learnJS("JavaScript", done  );
+// /////////////////////////////
+function first(one, callback){
+    setTimeout(function(){
+        console.log("Виводимо " + one);
+    },500 );
+    callback();
+}
+function second(){
+    console.log(2);
+}
+first(1, second);
