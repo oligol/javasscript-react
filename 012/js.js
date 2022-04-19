d287424fe007d88eb6661fee82278453f1a94fe7
oligol <oligol83@gmail.com>
2022-04-19 06:49:23 +0200

    014
    OOP Прототипи

--- 012/js.js
+++ 012/js.js
@@ -84,3 +84,19 @@ function compareNum(a,b) {
     return a-b;
 }
     console.log(arr);
+
+// OOP Прототип
+
+let soldier = {
+    health: 400,
+    armor: 100
+};
+
+let john = {
+    health: 100,
+};
+
+john.__proto__ =soldier;
+
+console.log(john);
+console.log(john.armor);
