7f17b5ebc3f068ac5431fac2c5b0e0da2f21e02e
oligol <oligol83@gmail.com>
2022-05-01 21:32:52 +0200

    018.1 Регулярні вирази

--- 018.1/script.js
+++ 018.1/script.js
@@ -35,4 +35,49 @@ window.addEventListener('DOMContentLoaded', function() {
         console.log("Red box: " + e.touches[0].pageX);
     });
     
+
+    // // new RegExp('pattern', 'flags');
+    // // /pattern/flags
+
+    // let ans = prompt('Введите ваше имя');
+
+    // let reg = /n/g;
+
+    
+    
+    // // search - слабкий 
+    // // console.log(ans.search(reg));
+    
+    
+    // // i Флаг Регістра
+    // // g Глобальний
+    // // m Багатострічковий
+
+
+    // \d клас   цифри \D Не цифра
+    // \w букви        \W Не буква
+    // \s пробіли      \S Не пробіл
+    // \/  \*   \% спеціальний символ
+
+    // console.log(ans.match(reg));
+
+    // // Заміна символів.
+
+    // let pass = prompt('Введите пароль');
+
+    // console.log(pass.replace(/./g, "*"));
+    // alert('12-34-56'.replace(/-/g, ':'));
+
+    // // Заміна символів.
+
+    // let ans = prompt('Введите число');
+
+    // let reg = /\d/g;
+
+    // console.log(ans.match(reg));
+
+    let str = 'My name is R2D2';
+
+    console.log(str.match(/\w\d\w\d/i));
+    
 });
