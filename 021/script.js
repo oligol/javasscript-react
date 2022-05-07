be046fe19474175409b986b6b6a9881bc35130ac
oligol <oligol83@gmail.com>
2022-05-07 09:22:47 +0200

    021 параметри вікна, документа і елемента

--- 021/script.js
+++ 021/script.js
@@ -1,17 +1,46 @@
 let box = document.querySelector('.box');
     btn = document.querySelector('button');
 
-let width = box.scrollWidth,
-    height = box.scrollHeight;
+let width = box.clientWidth,
+    height = box.clientHeight;
 
-// console.log(width);    
-// console.log(height); 
+console.log(width);    
+console.log(height);
+
+// Координати елемента
+console.log(box.getBoundingClientRect());
+
+console.log(box.getBoundingClientRect().left);
+
+// Координати елемента
+
+// Ширина і висота документа
+
+console.log(document.documentElement.clientWidth);
+
+console.log(document.documentElement.clientHeight);
 
 
+console.log(document.documentElement.scrollTop); //scroll
+// Ширина і висота документа
 
-// Прокрутка вгору сторінки
+// Прокрутка вгору елемента
 btn = addEventListener('click', function() {
     box.scrollTop = 0;
 });
 
+// Прокрутка вгору елемента
+
 // Прокрутка вгору сторінки
+
+document.documentElement.scrollTop = 0;
+
+// Прокрутка вгору сторінки
+
+
+// Прокрутка по сторінці
+
+scrollBy(0, 200);
+
+scrollTo(0, 500);  
+// Прокрутка по сторінці
