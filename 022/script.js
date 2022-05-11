e9b5113504473fcb5530488d3184128725adef09
oligol <oligol83@gmail.com>
2022-05-11 07:09:07 +0200

    023 dz
    this

--- 022/script.js
+++ 022/script.js
@@ -26,29 +26,43 @@
 // // ES5
 
 
-// ES6
+// // ES6
 
-class User {
-    constructor (name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
-    }
-    hello() {
-        console.log('Hello! ' + this.name );
-    }
-    exit() {
-        console.log('Пользователь ' + this.name, 'ушел');
-    }
+// class User {
+//     constructor (name, id) {
+//         this.name = name;
+//         this.id = id;
+//         this.human = true;
+//     }
+//     hello() {
+//         console.log('Hello! ' + this.name );
+//     }
+//     exit() {
+//         console.log('Пользователь ' + this.name, 'ушел');
+//     }
+// }
+// let ivan = new User('Ivan', 25),
+//      alex = new User('Alex', 20);
+
+// console.log(ivan);
+// console.log(alex);
+
+// ivan.hello();
+// alex.exit();
+
+// // ES6
+
+let age = document.getElementById('age');
+
+
+
+function ShowUser(surname, name) {
+  this.surname = surname;
+  this.name = name;
+  this.value = age.value;
+
+  alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
-let ivan = new User('Ivan', 25),
-     alex = new User('Alex', 20);
-
-console.log(ivan);
-console.log(alex);
-
-ivan.hello();
-alex.exit();
-
-// ES6
+ShowUser('Bigest', 'Tom');
+ShowUser('Big', 'Tm');
 
