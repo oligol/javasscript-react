31095994292bd1c3f56a0dd90e462da28d997a35
oligol <oligol83@gmail.com>
2022-05-12 06:26:00 +0200

    024 ES6 Інтерполяція

---
+++ 024/script.js
@@ -0,0 +1,12 @@
+let name = "Ivan",
+    age = 30,
+    mail = "ex@mail.ru";
+
+document.write('Пользователю ' + name + " " + age + ' лет. Его почтовый адрес ' + mail);
+
+
+// интерполяция ES6
+
+document.write(`Пользователю  ${name} ${age} лет. Его почтовый адрес ${mail}`);
+
+
