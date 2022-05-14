6b0b736f0da91c9b00625ea00fae5e33574a2864
oligol <oligol83@gmail.com>
2022-05-14 21:09:37 +0200

    006 Оператори в JS

---
+++ 006/script.js
@@ -0,0 +1,83 @@
+//строгий режим
+a = 5;
+console.log(a);
+
+// 
+'use strict';
+
+a = 5;
+console.log(a);
+
+
+// console.log("arr" + " - object");
+
+
+// //
+
+// console.log(5 + " - object");
+
+// //
+
+
+// let answer = +prompt('Есть ли вам 18?', "Да");
+
+// console.log(typeof(answer));
+// //
+
+// // інкимент, декримент
+// let incr = 10,
+//     decr = 10;
+
+//     //постфіксна форма
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+
+// //
+
+let incr = 10,
+    decr = 10;
+
+    //префіксна форма
+
+
+console.log(++incr);
+console.log(--decr);
+
+
+
+//залишок від ділення
+
+console.log(5%2);
+
+
+
+// рівність = - присвоєння    == - перевірка на рівність  === - строга перевірка по типу данних
+
+console.log("2" == 2);
+
+console.log("2" === 2);
+
+// // оператор "і" &&  та "або" || 
+
+// let isChecked = true,
+//     isClose = true;
+//     closed = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(closed || isClose);
+
+// оператор нерівності !
+
+
+let isChecked = true,
+    isClose = true;
+    closed = false;
+
+console.log(!isClose);
+console.log(isClose);
+
