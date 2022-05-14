68fb5469712b6211c84694f826de7f3e92fea996
oligol <oligol83@gmail.com>
2022-05-14 16:59:25 +0200

    005 Простое общение с пользователем

---
+++ 005/script.js
@@ -0,0 +1,18 @@
+// alert("Hello World");
+
+
+
+// confirm("Are you here?"); // ok or cancel
+
+
+
+
+// let answer = confirm("Are you here?"); // ok = true,  cancel = false
+
+// console.log(answer); // ok = true,  cancel = false
+
+
+
+let answer = prompt("Есть ли вам 18?", "Да"); // 
+
+console.log(answer); // cancel = null
