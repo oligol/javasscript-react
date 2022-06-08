0e1df114d3e8bdaad735c2ebb583aaa99f8b0b22
oligol <oligol83@gmail.com>
2022-06-08 19:58:43 +0200

    BugFix

--- 008/script.js
+++ 008/script.js
@@ -4,7 +4,7 @@
 if (2*4 == 9) { // в дужках перетворюється в булінове значення.
     console.log("Правда");
 } else {
-    console.log("Не правельно");
+    console.log("Wrong");
 }
 
 
@@ -26,13 +26,14 @@ let num = 50;
 
 switch (num) {
     case num <49:
-        console.log("Не верно");
+        console.log("Wrong");
         break;
     case num > 100:
         console.log("Много");
         break;
     case num > 80:
-        console.log("Все еще Много");
+        console.log("Еще Много");
+        break;
     case 50: // num == 50: не правельний запис
         console.log("Верно");
         break;
