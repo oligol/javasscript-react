8a5dc168930153726c3e39db64db819e4aee34d9
oligol <oligol83@gmail.com>
2022-06-08 20:12:52 +0200

    030 JSON

---
+++ 030 JSON/script.js
@@ -0,0 +1,15 @@
+let options = {
+    width: 1366,
+    height: 768,
+    background: 'red',
+    font: {
+        size: '16px',
+        color: '#fff'
+    }
+};
+
+
+console.log(JSON.stringify(options)); //( вивантаження - stringify)
+
+
+console.log(JSON.parse(JSON.stringify(options))); //( завантаження - parse)
